522b8583e9966d2a6cf05f944ef33fb92f0d4057
malireza1 <marefat_dostan20@yahoo.com>
2023-08-21 18:38:20 -0700

    new runner added

--- src/test/java/api/runner/RegressionRunner.java
+++ src/test/java/api/runner/RegressionRunner.java
@@ -2,16 +2,12 @@ package api.runner;
 
 import com.intuit.karate.junit5.Karate;
 
-public class TestRunner {
-
-	
+public class RegressionRunner {
 	@Karate.Test
 	public Karate runTest() {
 		
 		return Karate.run("classpath:features")
-				.tags("@Smoke");
+				.tags("Regression");
 	}
 
-
-	
 }
